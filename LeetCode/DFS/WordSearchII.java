f5835847dbc3d1118a8ab2ab032c053549c4ceb8
Sripathi Bhat <shripathisimha@gmail.com>
2021-10-10 13:33:51 +0530

    Word search II efficient solution using Trie and DFS

--- LeetCode/DFS/WordSearchII.java
+++ LeetCode/DFS/WordSearchII.java
@@ -46,3 +46,65 @@ class Solution {
         return false;
     }
 }
+
+
+
+// Efficient solution - Trie + DFS
+// Create a trie for all given words, then do DFS iff trie contains the character which we are currently iterating over the grid.
+// Reference - Coding decoded github
+class Solution {
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> res = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+        int m = board.length, n = board[0].length;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                dfs(board, i, j, root, res);
+            }
+        }
+        return res;
+    }
+
+    private void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
+        char c = board[i][j];
+        if (c == ';' || p.children[c - 'a'] == null) return;
+        p = p.children[c - 'a'];
+        if (p.word != null) {
+            res.add(p.word);
+            p.word = null;     // de-duplicate
+        }
+
+        board[i][j] = ';';
+        if (i > 0) dfs(board, i - 1, j ,p, res);
+        if (j > 0) dfs(board, i, j - 1, p, res);
+        if (i < board.length - 1) dfs(board, i + 1, j, p, res);
+        if (j < board[0].length - 1) dfs(board, i, j + 1, p, res);
+        board[i][j] = c;
+    }
+    
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for(String word : words) {
+            TrieNode p = root;
+            for(char c : word.toCharArray()) {
+                int i = c - 'a';
+                if (p.children[i] == null) {
+                    p.children[i] = new TrieNode();
+                }
+                p = p.children[i];
+            }
+            p.word = word;
+        }
+        return root;
+    }
+}
+
+class TrieNode {
+    String word;
+    TrieNode children[];
+
+    TrieNode() {
+        children = new TrieNode[26];
+    }
+}
