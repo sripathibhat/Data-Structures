00b26a1ce7925b336bc2a5c6068f30a2ce5f3b4b
SRIPATHI BHAT <shripathisimha@gmail.com>
2021-10-26 12:13:12 +0530

    Invert tree and find largest value in each tree row

---
+++ LeetCode/Binary Tree and BST/FindLargestInEachTreeRow.java
@@ -0,0 +1,48 @@
+/*
+
+    Given the root of a binary tree, return an array of the largest value
+    in each row of the tree (0-indexed).
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root == null) {
+            return res;
+        }
+        q.add(root);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            int max = Integer.MIN_VALUE;
+            for(int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                max = Math.max(max, node.val);
+                if(node.left != null) {
+                    q.add(node.left);
+                }
+                if(node.right != null) {
+                    q.add(node.right);
+                }
+            }
+            res.add(max);
+        }
+        return res;
+    }
+}
