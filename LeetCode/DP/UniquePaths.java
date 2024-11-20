e03a11c4a1d6dd3c28ca509ce3c2a80fdaf4473b
SRIPATHI BHAT <shripathisimha@gmail.com>
2024-11-20 10:01:42 +0530

    Update UniquePaths

--- LeetCode/DP/UniquePaths.java
+++ LeetCode/DP/UniquePaths.java
@@ -11,16 +11,39 @@
 
 class Solution {
     public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m][n];
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(i == 0 || j == 0) {
-                    dp[i][j] = 1;
-                } else {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-                }
-            }
+        // bottom up DP or tabulation method
+        // int dp[][] = new int[m][n];
+        // for(int i = 0; i < m; i++) {
+        //     for(int j = 0; j < n; j++) {
+        //         if(i == 0 || j == 0) {
+        //             dp[i][j] = 1;
+        //         } else {
+        //             dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        //         }
+        //     }
+        // }
+        // return dp[m - 1][n - 1];
+
+        // top down DP or memoization
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        return dp[m - 1][n - 1];
+        return solve(m - 1, n - 1, m, n, dp);
+    }
+
+    private int solve(int i, int j, int m, int n, int dp[][]) {
+        if (i == 0 || j == 0) {
+            return 1;
+        }
+        if (i < 0 || i > m - 1 || j < 0 || j > n - 1) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int fromAbove = dp[i - 1][j] != -1 ? dp[i - 1][j] : solve(i - 1, j, m, n, dp);
+        int fromLeft = dp[i][j - 1] != -1 ? dp[i][j - 1] : solve(i, j - 1, m, n, dp);
+        dp[i][j] = fromLeft + fromAbove;
+        return dp[i][j];
     }
 }
