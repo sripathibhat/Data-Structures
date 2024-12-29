f2c9bdbfafe4ce72276cff17fbc44deb4dd63fb0
SRIPATHI BHAT <shripathisimha@gmail.com>
2024-12-29 22:31:21 +0530

    Update StringToInteger

--- LeetCode/Strings/StringToInteger.java
+++ LeetCode/Strings/StringToInteger.java
@@ -82,12 +82,12 @@ Reading stops at the first non-digit character 'w'.
 class Solution {
     public int myAtoi(String s) {
         s = s.trim();
-        if (s.equals("") || (!isDigit(s.charAt(0)) && s.charAt(0) != '-' && s.charAt(0) != '+')) {
+        if (s.equals("") || (!Character.isDigit(s.charAt(0)) && s.charAt(0) != '-' && s.charAt(0) != '+')) {
             return 0;
         }
         String res = "";
-        for (int i=0; i<s.length(); i++) {
-            if (isDigit(s.charAt(i)) || (i == 0 && (s.charAt(i) == '-' || s.charAt(i) == '+'))) {
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i)) || (i == 0 && (s.charAt(i) == '-' || s.charAt(i) == '+'))) {
                 res = res + s.charAt(i);
             } else {
                 break;
@@ -95,14 +95,19 @@ class Solution {
         }
         if (res.equals("+") || res.equals("-")) 
             return 0;
-        try {
-            return Integer.valueOf(res);
-        } catch (NumberFormatException e) {
-            return res.charAt(0) != '-' ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        if (res.charAt(0) == '-') {
+            if (Double.parseDouble(res) < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
         }
-    }
-    
-    private boolean isDigit(char c) {
-        return (c >= '0' && c <= '9');
+        if (Double.parseDouble(res) > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        return Integer.valueOf(res);
+        // try {
+        //     return Integer.valueOf(res);
+        // } catch (NumberFormatException e) {
+        //     return res.charAt(0) != '-' ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        // }
     }
 }
