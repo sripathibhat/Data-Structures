96ad7ddca6b57f1ea504ccdae7818d8759efcf49
SRIPATHI BHAT <shripathisimha@gmail.com>
2025-01-06 11:24:05 +0530

    Update LongestValidParentheses

--- LeetCode/Stack/LongestValidParentheses.java
+++ LeetCode/Stack/LongestValidParentheses.java
@@ -23,16 +23,52 @@ Output: 0
 class Solution {
     public int longestValidParentheses(String s) {
         int res = 0;
-        Stack<Integer> st = new Stack<>();
-        st.push(-1);
+
+        // Extra space
+        // Stack<Integer> st = new Stack<>();
+        // st.push(-1);
+        // for (int i = 0; i < s.length(); i++) {
+        //     if (s.charAt(i) == ')' && st.peek() != -1 && s.charAt(st.peek()) == '(') {
+        //         st.pop();
+        //         res = Math.max(res, i - st.peek());
+        //     } else {
+        //         st.push(i);
+        //     }
+        // }
+
+        // Optimized - No extra space
+        int left_count = 0;
+        int right_count = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ')' && st.peek() != -1 && s.charAt(st.peek()) == '(') {
-                st.pop();
-                res = Math.max(res, i - st.peek());
+            if (s.charAt(i) == '(') {
+                left_count++;
             } else {
-                st.push(i);
+                right_count++;
+            }
+            if (left_count == right_count) {
+                res = Math.max(res, left_count + right_count);
+            } else if (left_count < right_count) {
+                left_count = right_count = 0;
             }
         }
+
+        left_count = right_count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left_count++;
+            } else {
+                right_count++;
+            }
+            if (left_count == right_count) {
+                res = Math.max(res, left_count + right_count);
+            } else if (left_count > right_count) {
+                left_count = right_count = 0;
+            }
+        }
+
+
         return res;
     }
 }
+
