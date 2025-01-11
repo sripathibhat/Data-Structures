de8f22674459faa57fb965cda37db6ec066531f4
SRIPATHI BHAT <shripathisimha@gmail.com>
2025-01-11 17:25:24 +0530

    Update FirstMissingPositive

--- LeetCode/Arrays/FirstMissingPositive.java
+++ LeetCode/Arrays/FirstMissingPositive.java
@@ -26,22 +26,19 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         int i = 0;
         while (i < nums.length) {
-            if (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i]-1] != nums[i]) {
-                int t = nums[nums[i]-1];
-                nums[nums[i]-1] = nums[i];
+            if (nums[i] >= 1 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+                int t = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
                 nums[i] = t;
             } else {
                 i++;
             }
         }
-        boolean positive = false;
-        for (i = 0; i < nums.length; i++) {
-            if (!positive && nums[i] > 0) 
-                positive = true;
-            if (nums[i] != i + 1) 
-                return i + 1;
+        for (i = 1; i <= nums.length; i++) {
+            if (nums[i - 1] != i) 
+                return i;
             
         }
-        return positive ? nums.length + 1 : 1;
+        return nums.length + 1;
     }
 }
