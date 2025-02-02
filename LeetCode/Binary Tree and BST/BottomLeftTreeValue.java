124c1a336381e6a74d073c52c903fe8338556e02
SRIPATHI BHAT <shripathisimha@gmail.com>
2025-02-02 23:15:06 +0530

    Create BottomLeftTreeValue

---
+++ LeetCode/Binary Tree and BST/BottomLeftTreeValue.java
@@ -0,0 +1,53 @@
+/*
+
+Given the root of a binary tree, return the leftmost value in the last row of the tree.
+
+Example 1:
+
+Input: root = [2,1,3]
+Output: 1
+Example 2:
+
+
+Input: root = [1,2,3,4,null,5,6,null,null,7]
+Output: 7
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList();
+        q.add(root);
+        int res = root.val;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            TreeNode node = q.peek();
+            res = node.val;
+            for (int i = 0; i < size; i++) {
+                node = q.poll();
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+        }
+        return res;
+    }
+}
